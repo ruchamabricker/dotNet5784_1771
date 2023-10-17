b1a60d7d5f5d3e2225a930dd1fb7cd64b9cf10e4
ruchamabricker <ruchamabricker@gmail.com>
2023-10-17 10:32:15 +0300

    Intial Code + Welcome functions

---
+++ Stage0/Program1771.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome1771();
+            Welcome1771_2();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1771_2();
+
+        private static void Welcome1771()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
