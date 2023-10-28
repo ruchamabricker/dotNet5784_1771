8169fb1c28f4d66e59d4257eab5ea0badf3ae56f
ruchamabricker <ruchamabricker@gmail.com>
2023-10-29 00:25:34 +0300

    אמצע שלב 1 פרק 7

---
+++ DalList/EnigneerImplementation.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Dal;
+using DalApi;
+using DO;
+using System.Collections.Generic;
+
+public class EnigneerImplementation : IEnigneer
+{
+    
+    public int Create(Enigneer item)
+    {
+        if (item == null)
+            throw new NotImplementedException();
+        
+        DataSource.Enigneers.Add(item);
+        return item.id;
+    
+    }
+
+    public void Delete(int id)
+    {
+        //if (!DataSource.Enigneers.Contains(item=>id))
+
+            DataSource.Enigneers.Find(item=>id==item.id);
+
+            throw new NotImplementedException();
+    }
+
+    public Enigneer? Read(Enigneer id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Enigneer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Enigneer item)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DalList/ListImplementation.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListImplementation : ITask
+{
+    public int Create(Task item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task? Read(Task id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Task> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Task item)
+    {
+        throw new NotImplementedException();
+    }
+}
